563b8e54d06bb92907d22f91fb2f478de2f91cb4
Emmanuel Akinyele <emmanuel.akinyele@andela.com>
2016-08-15 14:48:58 +0100

    Update configuration and build task for production

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,34 +1,37 @@
-var path = require('path'),
-    webpack = require('webpack');
-    
-module.exports = {
-  // path to the javascript file that serves as the entry point to our
-  // React application.
-  entry: [
-    './src/index.js'
-  ],
+const webpack = require('webpack');
+const path = require('path');
 
-  // path to output our bundled file.
+ module.exports = {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  noInfo: false,
+  entry: [
+    'eventsource-polyfill', //important for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    './app/js/index.js'
+  ],
+  target: 'web', // bundle app the way web browsers can understand
   output: {
-    path: __dirname,
+    path: __dirname + '/app', // Note: Physical files are only output by the production build task `npm run build`
     publicPath: '/',
     filename: 'bundle.js'
   },
-
-  // we're running babel loader, converting our ES6 & JSX to browser-compatible ES5
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
-      }
-    }]
+  devServer: {
+    contentBase: './app'
   },
-
-  // allows us to shorten files when using require * require('fileName') instead of require('fileName.jsx')
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  }
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'app'), loaders: ['babel']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
+
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,25 +1,48 @@
-var path = require('path'),
-    webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    devtool: 'source-map',
+const GLOBALS = {
+    'process.env.NODE_ENV': JSON.stringify('production')
+};
 
+module.exports =  {
+    debug: true,
+    devtool: 'cheap-module-eval-source-map',
+    noInfo: false,
+    target: 'web', // bundle app the way web browsers can understand
     entry: [
-        path.join(__dirname, 'app', 'js', 'app.js')
+        path.join(__dirname, 'app', 'js', 'index.js')
     ],
 
     output: {
         path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
-        publicPath: '/build/'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
 
+    devServer: {
+        contentBase: './build'
+    },
     plugins: [
-
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin(GLOBALS),
+        new ExtractTextPlugin('styles.css'),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
     ],
-
     module: {
         loaders: [
+            { test: /\.js$/, include: path.join(__dirname, 'app'), loaders: ['babel'] },
+            { test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap") },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+            { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
         ]
     }
-}
+};
+
+
+
+
