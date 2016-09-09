fa697a4f7bcac113943a8f13f4f262da9270dfd5
Olajide <olajide.aderibigbe@andela.co>
2016-09-09 09:00:00 +0100

    Added Nav bar links

--- src/js/components/header/Index.js
+++ src/js/components/header/Index.js
@@ -5,20 +5,27 @@ const Header = () => {
   return (
     <nav className="navbar navbar-default navbar-fixed-top top-header">
       <div className="container-fluid">
-        <div className="navbar-header">
-          <Link to="/" className="navbar-brand">
-            Hidden Wisdom
-          </Link>
-        </div>
-        <div className="be-right-navbar">
-          <ul className="nav navbar-nav navbar-right user-nav">
-            <li className="dropdown">
-              <Link to="/proverbs/new" role="button" className="btn btn-primary">
-                Add a new proverb
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <Link to="/" className="navbar-brand">Hidden Wisdom</Link>
+        <ul className="nav navbar-nav pull-xs-right right-nav">
+          <li className="nav-item" key={1}>
+            <Link className="nav-link" to="/proverbs/favorites">
+              <i className="fa fa-star" aria-hidden="true"></i>
+              Favorites
+            </Link>
+          </li>
+          <li className="nav-item" key={2}>
+            <Link className="nav-link" to="/proverbs">
+              <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+              Edit
+            </Link>
+          </li>                  
+          <li className="nav-item" key={3}>
+            <Link className="nav-link" to="/proverbs/new">
+              <i className="fa fa-file-o" aria-hidden="true"></i>
+              Add
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
