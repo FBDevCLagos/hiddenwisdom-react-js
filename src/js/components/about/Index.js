b4a27ea375c270efb7b60cb3a3d4f9929cf1789b
Innocent <innocent.amadi@andela.com>
2017-01-18 12:56:41 +0100

    bundle js

--- src/js/components/about/Index.js
+++ src/js/components/about/Index.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react'; // eslint-disable-line no-unused-vars
 import text from './text';
 
+/*eslint-disable react/no-danger*/
+
 const About = () => {
   return (
     <div className="panel-container">
@@ -8,12 +10,14 @@ const About = () => {
       <div className="about-page row">
         <div className="content col-8 col-md-8 offset-md-2">
           {text.quotes.map(({quote, author, desc}) => {
-            return <div>
-              <blockquote className="blockquote">{quote}
-                <span className="author">- {author}</span>
-              </blockquote>
-              <p dangerouslySetInnerHTML={{__html: desc}} />
-            </div>
+            return (
+              <div key={author}>
+                <blockquote className="blockquote">{quote}
+                  <span className="author">- {author}</span>
+                </blockquote>
+                <p dangerouslySetInnerHTML={{__html: desc}} />
+              </div>
+            )
           })}
         </div>
       </div>
